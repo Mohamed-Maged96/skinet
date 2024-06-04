92cb1c5b687e4252cfbb83cdf06177e7fd772494
Mohamed-Maged96 <mohamedmaged291196@gmail.com>
2024-06-04 12:28:23 +0300

    End of Section 10

--- client/src/app/shop/shop-routing.module.ts
+++ client/src/app/shop/shop-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+
 import { ShopComponent } from './shop.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 const routes: Routes = [
-  {path: 'shop', component: ShopComponent}
+  {path: '', component: ShopComponent},
+  {path: ':id', component: ProductDetailsComponent}
 ];
 
 @NgModule({
