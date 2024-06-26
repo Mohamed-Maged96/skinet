2a5b949cc3f8a7d9f3fdbf827ab9ef2456673911
Mohamed-Maged96 <mohamedmaged291196@gmail.com>
2024-06-26 15:07:55 +0300

    End of section 13

--- API/Helpers/ProductUrlResolver.cs
+++ API/Helpers/ProductUrlResolver.cs
@@ -11,7 +11,6 @@ namespace API.Helpers
         public ProductUrlResolver(IConfiguration config)
         {
             _config = config;
-          
         }
 
         public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
